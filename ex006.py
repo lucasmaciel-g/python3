71453920580691abbaa23b10e0f2a777e02cb5a8
Lucas Maciel Guimarães <lucasmaciel395@gmail.com>
2025-02-11 22:04:12 -0300

    Dobro, triplo e raiz quadrada
    O desafio aqui era ler um numero inserido pela pessoa, calcular o dobro, o triplo e a raiz quadrada. Alem disso, coloquei limitação de quantos números queria após a virgula, ou seja, quantos pontos flutuantes.

---
+++ ex006.py
@@ -0,0 +1,2 @@
+n1 = int(input('Insira aqui um numero qualquer que te falo o dobro, o triplo e a raiz quadrada desse numero: '))
+print('o numero escolhido foi {}, o dobro dele é {}, o triplo é {} e a sua raiz quadrada é {:.0f}.'.format(n1, n1*2, n1*3, n1**(1/2)))
