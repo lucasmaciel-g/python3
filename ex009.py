3a0deb9e675e1730b52559a14de47f33c850886a
Lucas Maciel Guimarães <lucasmaciel395@gmail.com>
2025-02-11 23:40:59 -0300

    Exercício 009 - Tabuada
    Criação de uma tabuada de multiplicação. A pessoa insere um numero e o programa retorna com a tabuada completa de 1 a 10.

---
+++ ex009.py
@@ -0,0 +1,2 @@
+n = int(input('Vamos aprender a tabuada! Agora, insira um numero que te devolvo com a tabuada de multiplicação dele: '))
+print(' {} x 1 = {} \n {} x 2 = {} \n {} x 3 = {} \n {} x 4 = {} \n {} x 5 = {} \n {} x 6 = {} \n {} x 7 = {} \n {} x 8 = {} \n {} x 9 = {} \n {} x 10 = {}'.format(n, n*1, n, n*2, n, n*3, n, n*4, n, n*5, n, n*6, n, n*7, n, n*8, n, n*9, n, n*10))
